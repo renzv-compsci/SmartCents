9aa7da54bb61e61635795605e0e51d233d2112b6
renzv-compsci <viloriarenz917@gmail.com>
2025-07-17 22:19:08 +0800

    Add sample controller for testing

---
+++ SmartCents/src/main/java/com/smartcents/SmartCentsApplication.java
@@ -0,0 +1,11 @@
+package com.smartcents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCentsApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(SmartCentsApplication.class, args);
+    }
+}
