8ddc13b14e6e832bb2a1aebf9dbcbe95b98271af
renzv-compsci <viloriarenz917@gmail.com>
2025-07-18 21:53:00 +0800

    Update test

--- SmartCents/src/test/java/com/smartcents/AppTest.java
+++ SmartCents/src/test/java/com/smartcents/AppTest.java
@@ -1,38 +1,12 @@
 package com.smartcents;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    void sampleTest() {
+        assertTrue(1 + 1 == 2, "1 + 1 should equal 2");
     }
 }
